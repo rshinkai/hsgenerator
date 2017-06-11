f0b10b4206bf55c905b75ba60d1bfebc6e1eb168
koguma <aaaaa_33@excite.co.jp>
2017-06-11 23:29:25 +0900

    bootstrap追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'hello/index'
+  get 'hello/index' #hello コントローラーのindexアクションを実行するURLを作成
   get 'hello', to: 'hello#index'
   post 'hello', to: 'hello#index'
   post 'hello/index'
