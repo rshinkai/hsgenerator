4cffcb9adff1ef940955c8a7ea9e10d85fc924c0
koguma <aaaaa_33@excite.co.jp>
2017-06-13 19:06:03 +0900

    ロゴ配置、背景をそれっぽく配色、ヘッダー修正

--- app/controllers/create_controller.rb
+++ app/controllers/create_controller.rb
@@ -1,4 +1,24 @@
 class CreateController < ApplicationController
   def index
   end
+
+# ここから、フォームのサンプル（一応コメントアウトして残す）
+=begin
+  protect_from_forgery
+
+
+  def form_sample
+      if request.post? then
+        @title = 'Result'
+        @msg = 'you typed:' + params['input1'] + '.'
+        @value = params['input1']
+      else
+        @title = 'Index'
+        @msg = 'type text...'
+        @value = ''
+      end
+  end
+=end
+# ここまで
+
 end
