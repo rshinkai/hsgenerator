4a41cb9a27154bec949e1c6beab26db20ca7878e
koguma <aaaaa_33@excite.co.jp>
2017-06-13 17:47:14 +0900

    初期画面のパスを修正（localhost:3000で、表示）、モデルを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get '/' => "create#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'hello/index' #hello コントローラーのindexアクションを実行するURLを作成
-  get 'hello', to: 'hello#index'
-  post 'hello', to: 'hello#index'
-  post 'hello/index'
+  #get 'hello/index' #hello コントローラーのindexアクションを実行するURLを作成
+  #get 'hello', to: 'hello#index'
+  #post 'hello', to: 'hello#index'
+  #post 'hello/index'
 end
