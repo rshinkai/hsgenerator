2bb9a0e7ee256d5b0512494f27f6b4448f46908c
Miura Eiji <guitar.pg.sc@gmail.com>
2017-07-22 17:13:19 +0900

    Refactored the routing and link to the root path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => "create#index"
+  root to: "create#index"
   post 'create', to: 'create#createCard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get 'hello/index' #hello コントローラーのindexアクションを実行するURLを作成
