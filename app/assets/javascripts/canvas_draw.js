fb5eab859e7f2365f79990721ce07e90d727748f
Ryo <ryo.shinkai1991@gmail.com>
2017-07-23 00:25:55 +0900

    コスト、アタック、ヘルス入力で、画像に同時書き込み処理追加

--- app/assets/javascripts/canvas_draw.js
+++ app/assets/javascripts/canvas_draw.js
@@ -4,37 +4,77 @@ var gemsrc = '/gems/gem_';
 var gemName = 'common';
 var extention = '.png';
 
+var cost = null;
+var attack = null;
+var health = null;
+
 $(document).ready(function() {
-  drawCard(templateName,gemName);
+  drawCard();
 });
 
-function drawCard(templateName, gemName){
+// クラス、レア度を選択した場合の処理
+$(function(){
+  $('select[name="selection[0]"]').change(function(){
+    drawCard();
+  });
+});
+
+// テキストエリアを入力した場合の処理
+$(function(){
+  $('.inputarea').change(function(){
+    drawCard();
+  });
+});
+
+function drawCard(){
   var canvas = document.getElementById('card');
   var ctx = canvas.getContext('2d');
+
+  var templateName = $('.cardclass option:selected').text();
+  var gemName = $('.rarity option:selected').text();
+
+  var imgArray = new Array();
 
   // card-template
   var cardTemplate = new Image();
   cardTemplate.src = imgsrc + templateName + extention;
+  imgArray.push(cardTemplate.src);
 
   // gem
   var gem = new Image();
   gem.src = gemsrc + gemName + extention;
+  imgArray.push(gem.src);
 
-  cardTemplate.onload = function(){
-    ctx.drawImage(cardTemplate, 0 , 0);
-    ctx.drawImage(gem, 195, 310, 30, 35);
-  }
-  gem.onload = function(){
-    ctx.drawImage(cardTemplate, 0 , 0);
-    ctx.drawImage(gem, 195, 310, 30, 35);
+  // cost
+  var cost = $("#card_cost").val();
+
+  // attack
+  var attack = $("#card_attack").val();
+
+  // health
+  var health = $("#card_health").val();
+
+  // 読み込んだ画像数カウント
+  var imgCnt = 0;
+  // イメージオブジェクト
+  var Img = new Array();
+  // カード枠とジェム画像が読み込まれたら、canvax描画
+  for(i = 0; i < imgArray.length; i++){
+    var img = new Image();
+    img.src = imgArray[i];
+    img.onload = function() {
+      // 読み込んだカウントアップ
+      imgCnt++;
+      if(imgCnt == imgArray.length){
+        ctx.drawImage(cardTemplate, 0 , 0);
+        ctx.drawImage(gem, 195, 310, 30, 35);
+        ctx.font='normal bold 60px sans-serif';
+        ctx.fillStyle = '#fff';
+        ctx.fillText(cost, 45,80);
+        ctx.font='normal bold 48px sans-serif';
+        ctx.fillText(attack, 50,505);
+        ctx.fillText(health, 335,510);
+      }
+    }
   }
 }
-
-$(function(){
-  $('select[name="selection[0]"]').change(function(){
-    // 選択されている表示文字列を取り出す
-    var templateName = $('.cardclass option:selected').text();
-    var gemName = $('.rarity option:selected').text();
-    drawCard(templateName,gemName);
-  });
-});
